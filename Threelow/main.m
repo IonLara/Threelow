d73d4cc2926d14e6dd120adb04a5c466af4f2577
SebasMac <ionrodriguezlara@gmail.com>
2023-09-07 20:29:13 -0700

    First Commit

---
+++ Threelow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Ion Sebastian Rodriguez Lara on 07/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
